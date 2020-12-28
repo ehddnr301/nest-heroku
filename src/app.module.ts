c33db9e222d5c91d83d1cccb87d3c8d924b0d95a
ehddnr301 <dy950328@gmail.com>
2020-12-28 20:42:18 +0900

    feat : check db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,16 +10,19 @@ import { ModelModule } from './model/model.module';
     ModelModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.env.test',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: true,
-      logging: true,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
     }),
   ],
   controllers: [AppController],
