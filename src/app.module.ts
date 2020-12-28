adc375b94b2e5f190968d11a6eca4cfe976bab00
ehddnr301 <dy950328@gmail.com>
2020-12-28 20:54:58 +0900

    feat : ssl true option

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { ModelModule } from './model/model.module';
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
+            ssl: true,
           }),
     }),
   ],
