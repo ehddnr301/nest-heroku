2479f8f21a6e3c44a2f8cb6f3d019eba2276f299
ehddnr301 <dy950328@gmail.com>
2021-01-01 20:11:13 +0900

    feat : add problem entity

---
+++ src/problem/entities/problem.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+
+@Entity()
+export class Problem extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ nullable: false })
+  question!: string;
+
+  @Column({ nullable: true })
+  code: string;
+
+  @Column({ nullable: true })
+  option: string[];
+
+  @Column({ nullable: false })
+  answer!: string;
+}
